6d8c32d241ddeff2dc2d37738da8adf5f71bb314
rafaelgssa <rafael.gssa@pm.me>
2020-04-09 21:04:28 -0300

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,58 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-	entry: './src/index.tsx',
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.(j|t)sx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/env'],
+module.exports = (env, argv) => {
+	return {
+		entry: './src/index.tsx',
+		devtool: argv.mode === 'development' ? 'source-map' : false,
+		module: {
+			rules: [
+				{
+					test: /\.(j|t)sx?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env'],
+					},
 				},
-			},
-			{
-				enforce: 'pre',
-				test: /\.js$/,
-				loader: 'source-map-loader',
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.woff2?$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: 'fonts/',
+				{
+					enforce: 'pre',
+					test: /\.js$/,
+					loader: 'source-map-loader',
 				},
-			},
-		],
-	},
-	resolve: {
-		extensions: ['*', '.json', '.js', '.jsx', '.ts', '.tsx'],
-		alias: {
-			'react-dom': '@hot-loader/react-dom',
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.woff2?$/,
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts/',
+					},
+				},
+			],
 		},
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist/'),
-		publicPath: '/dist/',
-		filename: 'bundle.js',
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'public/'),
-		port: 3000,
-		publicPath: 'http://localhost:3000/dist/',
-		hotOnly: true,
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-	],
+		resolve: {
+			extensions: ['*', '.json', '.js', '.jsx', '.ts', '.tsx'],
+			alias: {
+				'react-dom': '@hot-loader/react-dom',
+			},
+		},
+		output: {
+			path: path.resolve(__dirname, 'docs/'),
+			publicPath: '/docs/',
+			filename: 'index.js',
+		},
+		devServer: {
+			contentBase: path.join(__dirname, 'public/'),
+			port: 3000,
+			publicPath: 'http://localhost:3000/docs/',
+			hotOnly: true,
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin(),
+		],
+	};
 };
